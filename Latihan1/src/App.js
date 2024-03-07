6841c90316465281c2302875d7c9d4d2e8477870
Mhmmdalfn1502 <malfan227@gmail.com>
2024-03-07 11:27:13 +0700

    rename

---
+++ Latihan1/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import HelloWorld from "./components/HelloWorld"
+import Expression from "./components/Expression"
+
+function App() {
+  return (
+    <>
+      <HelloWorld/>
+      <Expression/>
+    </>
+  );
+}
+
+export default App;
